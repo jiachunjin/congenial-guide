116d76b26d32c68a85b3e4921fbee5314957a212
jiachunjin <jiachun22.jin@gmail.com>
2025-11-26 13:22:47 +0000

    sync eval mme-p

--- runner/vq_distill/eval.py
+++ runner/vq_distill/eval.py
@@ -17,7 +17,7 @@ def img_describe():
     dtype = torch.bfloat16
 
     exp_dir = "/data/phd/jinjiachun/experiment/vq_llava_distill/1126_lfq_mlp_14_intern1B"
-    step = 8000
+    step = 10000
     config = OmegaConf.load(os.path.join(exp_dir, "config.yaml"))
     internvl_path = "/data/phd/hf_models/ckpt/OpenGVLab/InternVL3_5-1B"
     internvl = InternVLChatModel.from_pretrained(internvl_path)
@@ -26,7 +26,7 @@ def img_describe():
     ckpt_path = os.path.join(exp_dir, f"quantizer-vq_llava_distill-{step}")
     print(ckpt_path)
     ckpt = torch.load(ckpt_path, map_location="cpu", weights_only=True)
-    m, u = internvl.load_state_dict(ckpt, strict=False)
+    m, u = internvl.clip_quantizer.load_state_dict(ckpt, strict=True)
     print(f"missing keys: {m}, unmatched keys: {u}")    
 
     internvl = internvl.to(device, dtype).eval()
@@ -35,22 +35,87 @@ def img_describe():
     # ---------- chat with the model ----------
     image = "/data/phd/jinjiachun/codebase/ideal-octo-spork/asset/internet/messi_1.jpg"
     pixel_values = load_image(image, max_num=12).to(torch.bfloat16).cuda()
-
     question_prime = "<image>\n" + "Describe this image in great detail."
-    generation_config = dict(max_new_tokens=128, do_sample=True)
-    
+    generation_config = dict(max_new_tokens=256, do_sample=True)
     # extract visual features from pixel values
     vit_feature = internvl.get_vit_feature(pixel_values)
     print(pixel_values.shape, vit_feature.shape)
-
-    visual_features, code = internvl.lfq(vit_feature)
-
+    visual_features, code = internvl.clip_quantizer(vit_feature)
     generation_config["visual_features"] = visual_features
-
     response_raw = internvl.chat(tokenizer, pixel_values, question_prime, generation_config)
-    
     print(response_raw)
 
+@torch.no_grad()
+def test_mme():
+    from model.internvl.modeling_internvl_chat import InternVLChatModel
+    from runner.vq_distill.distill import add_quantizer
+
+    device = torch.device("cuda")
+    dtype = torch.bfloat16
+
+    # ---------- load trained internvl with new projector ----------
+    exp_dir = "/data/phd/jinjiachun/experiment/vq_llava_distill/1126_lfq_mlp_14_intern1B"
+    exp_name = exp_dir.split("/")[-1]
+    step = 10000
+    config = OmegaConf.load(os.path.join(exp_dir, "config.yaml"))
+    internvl_path = "/data/phd/hf_models/ckpt/OpenGVLab/InternVL3_5-1B"
+    internvl = InternVLChatModel.from_pretrained(internvl_path)
+    internvl = add_quantizer(internvl, config.model.quantizer)
+    
+    ckpt_path = os.path.join(exp_dir, f"quantizer-vq_llava_distill-{step}")
+    print(ckpt_path)
+    ckpt = torch.load(ckpt_path, map_location="cpu", weights_only=True)
+    m, u = internvl.clip_quantizer.load_state_dict(ckpt, strict=True)
+    print(f"missing keys: {m}, unmatched keys: {u}")    
+
+    internvl = internvl.to(device, dtype).eval()
+    tokenizer = AutoTokenizer.from_pretrained(internvl_path, trust_remote_code=True, use_fast=False)
+
+    data_files = {
+        "test": "/data/phd/data/dataset/benchmark/darkyarding/MME/data/test-*-of-*.parquet"
+    }
+    dataset = load_dataset("parquet", data_files=data_files)
+
+    for i, data in enumerate(dataset["test"]):
+        img_name = data["question_id"].split("/")[-1]
+        category = data["category"]
+        image = data["image"].convert("RGB")
+        question = data["question"]
+        gt_answer = data["answer"]
+
+        pixel_values = load_image(image, max_num=12).to(torch.bfloat16).cuda()
+
+        question_prime = '<image>\n' + question
+
+        generation_config = dict(max_new_tokens=50, do_sample=False)
+
+        # construct visual features
+        vit_feature = internvl.get_vit_feature(pixel_values)
+        visual_features, code = internvl.lfq(vit_feature)
+        generation_config["visual_features"] = visual_features
+
+        response_raw = internvl.chat(tokenizer, pixel_values, question_prime, generation_config)
+
+        answer = extract_yes_no_answer(response_raw)
+        print(response_raw, answer)
+        model_name = ckpt_path.split("/")[-1]
+        os.makedirs(f"evaluation/understanding/mme/{exp_name}_{step}", exist_ok=True)
+        with open(f"evaluation/understanding/mme/{exp_name}_{step}/{category}.txt", "a") as f:
+            line = f"{img_name}\t{question}\t{gt_answer}\t{answer}\n"
+            f.write(line)
+
+def extract_yes_no_answer(response_raw):
+    import re
+    response_lower = response_raw.lower().strip()
+    if re.search(r'\byes\b', response_lower):
+        response = "yes"
+    elif re.search(r'\bno\b', response_lower):
+        response = "no"
+    else:
+        # 如果没有找到yes/no，取第一个词
+        response = response_raw.split()[0].strip() if response_raw.split() else "unknown"
+    return response
 
 if __name__ == "__main__":
     img_describe()
+    test_mme()
