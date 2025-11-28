f81132204620c0734083b6605e6ae411b57d0c52
jiachunjin <jiachun22.jin@gmail.com>
2025-11-28 08:00:14 +0000

    add vit quantizer

--- model/quantizer/lfq.py
+++ model/quantizer/lfq.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
-
+from model.quantizer.vit import ViT
 
 def get_lfq_quantizer(config):
     if config.type == "MLP":
         return LFQ_MLP(config)
     elif config.type == "ViT":
-        raise NotImplementedError
+        return LFQ_ViT(config)
     else:
         raise NotImplementedError
 
@@ -38,3 +38,55 @@ class LFQ_MLP(nn.Module):
         x_vq = self.up_proj(feature_bin) # (B, 256, llm_hidden_size)
 
         return x_vq, p_
+
+class LFQ_ViT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.down_proj = ViT(config) # 4096 -> output_dim
+
+        self.up_proj = nn.Sequential(
+            nn.Linear(config.output_dim, 4 * config.llm_hidden_size),
+            nn.GELU(),
+            nn.Linear(4 * config.llm_hidden_size, config.llm_hidden_size),
+        )
+
+    def forward(self, x):
+        """
+        :param x: (B, 256, 4096), the input clip features
+        """
+        feature_low = self.down_proj(x) # (B, 256, output_dim)
+        p = torch.sigmoid(feature_low)
+        p_ = (p > 0.5).to(x.dtype)
+        feature_bin = p + (p_ - p).detach()
+
+        x_vq = self.up_proj(feature_bin) # (B, 256, llm_hidden_size)
+
+        return x_vq, p_
+
+    @torch.no_grad()
+    def get_bin_code(self, x):
+        feature_low = self.down_proj(x)
+        p = torch.sigmoid(feature_low)
+        code = (p > 0.5).to(x.dtype)
+
+        return code
+
+    @torch.no_grad()
+    def get_ids(self, x):
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    from omegaconf import OmegaConf
+
+    config = OmegaConf.load("config/vq_distill/distill_vit.yaml")
+
+    model = get_lfq_quantizer(config.model.quantizer)
+    x = torch.randn(2, 256, 4096)
+    x_vq, p_ = model(x)
+    
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Number of parameters: {num_params/1e6:.2f}M")
+    print(x_vq.shape, p_.shape)
---
+++ model/quantizer/vit.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+from model.quantizer.vit_basic import Block, precompute_freqs_cis_2d
+
+
+class ViT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.depth = config.depth
+        self.num_heads = config.num_heads
+        self.grid_size = config.grid_size
+
+        self.precompute_pos = dict()
+        self.input_proj = nn.Linear(config.input_feature_dim, config.hidden_size)
+        self.norm1 = nn.LayerNorm(config.hidden_size)
+        self.blocks = nn.ModuleList([Block(config.hidden_size, config.num_heads) for _ in range(config.depth)])
+        self.norm2 = nn.LayerNorm(config.hidden_size)
+        self.output_proj = nn.Linear(config.hidden_size, config.output_dim)
+
+    def fetch_pos(self, height, width, device):
+        if (height, width) in self.precompute_pos:
+            return self.precompute_pos[(height, width)].to(device)
+        else:
+            pos = precompute_freqs_cis_2d(self.hidden_size // self.num_heads, height, width).to(device)
+            self.precompute_pos[(height, width)] = pos
+            return pos
+
+    def forward(self, x):
+        """
+        x: [B, L, D], original vit feature
+        """
+        pos = self.fetch_pos(self.grid_size, self.grid_size, x.device)
+        B, L, D = x.shape
+
+        x = self.input_proj(x)
+        x = self.norm1(x).to(x.dtype)
+        for block in self.blocks:
+            x = block(x, pos)
+        x = self.norm2(x)
+        x = self.output_proj(x)
+
+        return x
+    
+    # def forward_ste(self, x):
+    #     x_low_dim = self.forward(x)
+    #     p = torch.sigmoid(x_low_dim)
+    #     p_ = (p > 0.5).to(x.dtype)
+    #     feature_bin = p + (p_ - p).detach() # (B, 256, 16)
+
+    #     return feature_bin
+    
+    # @torch.no_grad()
+    # def get_code(self, x):
+    #     x_low_dim = self.forward(x)
+    #     p = torch.sigmoid(x_low_dim)
+    #     p_ = (p > 0.5).to(x.dtype)
+
+    #     return p_
