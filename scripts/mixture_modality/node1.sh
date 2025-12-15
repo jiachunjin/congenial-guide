506b498e27d566a5c575dbe6abfd6a17207dcf17
jiachunjin <jiachun22.jin@gmail.com>
2025-12-15 12:02:06 +0000

    sync multi-node

---
+++ scripts/mixture_modality/node1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export NCCL_GDR_LEVEL=4
+export NCCL_IB_DISABLE=1
+
+# 多节点分布式训练环境变量
+export MASTER_ADDR=252.1.82.59
+export MASTER_PORT=19534
+export WORLD_SIZE=32
+export RANK=0
+export LOCAL_WORLD_SIZE=8
+
+accelerate launch \
+  --num_processes 32 \
+  --num_machines 4 \
+  --machine_rank 0 \
+  --main_process_ip 252.1.82.59 \
+  --main_process_port 19534 \
+  --config_file config/accelerate_config/multi_node.yaml \
+  runner/mixture_modality/moe.py \
+  --config config/mixture_modality/mot_multi_node.yaml
