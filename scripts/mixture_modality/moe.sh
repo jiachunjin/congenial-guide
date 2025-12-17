a7a5fcc33f262560574f4c5678168de7d411d1d9
Jiachun Jin <jiachun22.jin@gmail.com>
2025-12-17 05:23:21 +0000

    2 separate multi node exps

--- scripts/mixture_modality/moe.sh
+++ scripts/mixture_modality/moe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch \
+accelerate launch \
 --num_processes 8 \
 --config_file config/accelerate_config/deepspeed.yaml \
 runner/mixture_modality/moe.py \
